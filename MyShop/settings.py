7e46ab4247126fdb34282ac3db6d08e296a98dd9
nahthan123 <email@example.com>
2025-05-23 13:16:43 +0700

    fix: cấu hình lại DATABASES trong settings.py

--- MyShop/settings.py
+++ MyShop/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import dj_database_url
+import os
 
 from pathlib import Path
 import os
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-k@_vj-r*vmu#$f$)=%^^!$_^w(2%9#n(gz@&tka$@zql)#6o=0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['myshop-9vzq.onrender.com']
 
 
 # Application definition
@@ -82,6 +84,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 
 # Password validation
@@ -118,27 +125,54 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # settings.py
+# LOGIN_URL = 'login'
+# LOGOUT_REDIRECT_URL = 'product_list'
+# LOGIN_REDIRECT_URL = 'product_list'
+
+
+# import os
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+import os
+import dj_database_url
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# STATIC files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # nơi collectstatic sẽ gom các file
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # nơi chứa static gốc của bạn
+
+# MEDIA files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# settings.py
+# Authentication Redirects
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'product_list'
 LOGIN_REDIRECT_URL = 'product_list'
 
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# Database (Render sẽ set DATABASE_URL)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
